b495b4068553472fac58aebed634b0ebbf2fe758
Rahul Patil <81096780+RahulPatil-Tech@users.noreply.github.com>
2025-03-22 12:19:04 +0530

    Update BMWTest.java

--- OOPJ/Homework/BMWTest.java
+++ OOPJ/Homework/BMWTest.java
@@ -1,51 +1,57 @@
-class BMW {
-    public void showDetails() {
-        System.out.println("This is a BMW vehicle.");
+
+class Vehicle {
+    String type;
+
+    Vehicle() {
+        this("Unknown Vehicle");
+        System.out.println("Vehicle default constructor called.");
     }
 
-    public void maxSpeed() {
-        System.out.println("Speed varies by model.");
+    Vehicle(String type) {
+        this.type = type;
+        System.out.println("Vehicle parameterized constructor called with type: " + type);
     }
 }
 
-class BMWSeries3 extends BMW {
-    public void showDetails() {
-        System.out.println("This is a BMW Series 3.");
-    }
-    public void maxSpeed() {
-        System.out.println("Max speed of BMW Series 3 is 240 km/h.");
-    }
-}
-class BMWSeries5 extends BMW {
-    public void showDetails() {
-        System.out.println("This is a BMW Series 5.");
-    }
-    public void maxSpeed() {
-        System.out.println("Max speed of BMW Series 5 is 260 km/h.");
-    }
-}
-class BMWSeries7 extends BMW {
-    @Override
-    public void showDetails() {
-        System.out.println("This is a BMW Series 7.");
+class Car extends Vehicle {
+    String model;
+
+    Car() {
+        this("Generic Model");
+        System.out.println("Car default constructor called.");
     }
 
-    @Override
-    public void maxSpeed() {
-        System.out.println("Max speed of BMW Series 7 is 300 km/h.");
+    Car(String model) {
+        this.model = model;
+        System.out.println("Car parameterized constructor called with model: " + model);
     }
+
+    Car(String type, String model) {
+        super(type); 
+        this.model = model;
+        System.out.println("Car parameterized constructor called with type: " + type + " and model: " + model);
+    }
+
 }
 
-public class BMWTest {
+public class ConstructorChaining {
     public static void main(String[] args) {
-        BMW[] vehicles = new BMW[3];
-        vehicles[0] = new BMWSeries3();
-        vehicles[1] = new BMWSeries5();
-        vehicles[2] = new BMWSeries7();
-        for (BMW vehicle : vehicles) {
-            vehicle.showDetails();
-            vehicle.maxSpeed();
-            System.out.println();
-        }
+        Car car1 = new Car(); 
+        System.out.println("Details of car1:");
+        System.out.println("Type: " + car1.type + ", Model: " + car1.model);
+                    
+        System.out.println("--------------------");
+
+        Car car2 = new Car("Sedan");
+        System.out.println("Details of car2:");
+        System.out.println("Type: " + car2.type + ", Model: " + car2.model); 
+                                                                            
+        System.out.println("--------------------");
+
+        Car car3 = new Car("Electric", "Model X");
+        System.out.println("Details of car3:");
+        System.out.println("Type: " + car3.type + ", Model: " + car3.model);
+                                                                            
+        System.out.println("--------------------");
     }
 }
